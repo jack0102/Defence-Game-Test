e2a333547363d2f8be51ff46ae6f7c731f54d206
jack0102 <happycova1201@gmail.com>
2016-10-15 19:09:45 +0900

    add navigation

---
+++ Assets/Defence/Script/EnemyMoveNormal.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyMoveNormal : MonoBehaviour {
+
+	public Transform target;
+	NavMeshAgent nav;
+
+	// Use this for initialization
+	void Start () {
+		nav = GetComponent <NavMeshAgent> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		nav.SetDestination (target.position);
+	}
+}
